f17b3f76f065f25d1cd28570b8e14bfed5a5d988
revelli <gabriel.revelli@avisto.com>
2023-11-23 12:57:58 +0100

    :bug: fix aop issues

--- src/main/java/demo/avisto/spring/perfs/controller/DemoController.java
+++ src/main/java/demo/avisto/spring/perfs/controller/DemoController.java
@@ -23,56 +23,56 @@ public class DemoController {
     @GetMapping("/regions")
     @ResponseStatus(HttpStatus.OK)
     @TimeStatistic("/regions Request : {} ms")
-    List<RegionDtoOut> getRegions() {
+    public List<RegionDtoOut> getRegions() {
         return demoService.getRegions(RegionDtoOut::new);
     }
 
     @GetMapping("/departments")
     @ResponseStatus(HttpStatus.OK)
     @TimeStatistic("/departments Request : {} ms")
-    List<DepartmentDtoOut> getDepartments() {
+    public List<DepartmentDtoOut> getDepartments() {
         return demoService.getDepartments(DepartmentDtoOut::new);
     }
 
     @GetMapping("/cities")
     @ResponseStatus(HttpStatus.OK)
     @TimeStatistic("/cities Request : {} ms")
-    List<CityDtoOut> getCities() {
+    public List<CityDtoOut> getCities() {
         return demoService.getCities(CityDtoOut::new);
     }
 
     @GetMapping("/museums")
     @ResponseStatus(HttpStatus.OK)
     @TimeStatistic("/museums Request : {} ns")
-    List<MuseumDtoOut> getMuseums() {
+    public List<MuseumDtoOut> getMuseums() {
         return demoService.getMuseums(MuseumDtoOut::new);
     }
 
     @GetMapping("/regions/{inseeCode}")
     @ResponseStatus(HttpStatus.OK)
     @TimeStatistic("/region/inseeCode Request : {} ms")
-    RegionDtoOut getRegion(@PathVariable String inseeCode) {
+    public RegionDtoOut getRegion(@PathVariable String inseeCode) {
         return demoService.getRegionById(inseeCode, RegionDtoOut::new);
     }
 
     @GetMapping("/departments/{inseeCode}")
     @ResponseStatus(HttpStatus.OK)
     @TimeStatistic("/departments/inseeCode Request : {} ms")
-    DepartmentDtoOut getDepartment(@PathVariable String inseeCode) {
+    public DepartmentDtoOut getDepartment(@PathVariable String inseeCode) {
         return demoService.getDepartmentById(inseeCode, DepartmentDtoOut::new);
     }
 
     @GetMapping("/cities/{inseeCode}")
     @ResponseStatus(HttpStatus.OK)
     @TimeStatistic("/cities/inseeCode Request : {} ms")
-    CityDtoOut getCitie(@PathVariable String inseeCode) {
+    public CityDtoOut getCitie(@PathVariable String inseeCode) {
         return demoService.getCityById(inseeCode, CityDtoOut::new);
     }
 
     @GetMapping("/museums/{id}")
     @ResponseStatus(HttpStatus.OK)
     @TimeStatistic("/museums/id Request : {} ns")
-    MuseumDtoOut getMuseum(@PathVariable int id) {
+    public MuseumDtoOut getMuseum(@PathVariable int id) {
         return demoService.getMuseumById(id, MuseumDtoOut::new);
     }
 }
--- src/main/java/demo/avisto/spring/perfs/util/TimeCounter.java
+++ src/main/java/demo/avisto/spring/perfs/util/TimeCounter.java
@@ -1,31 +1,22 @@
 package demo.avisto.spring.perfs.util;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-
-import java.lang.reflect.Method;
 
 @Component
 @Aspect
 @Slf4j
 public class TimeCounter {
 
-    @SneakyThrows
-    @Around("@annotation(demo.avisto.spring.perfs.util.TimeStatistic)")
-    public void timeCounter(ProceedingJoinPoint pjp) {
+    @Around("@annotation(timeStatistic)")
+    public Object timeCounter(ProceedingJoinPoint pjp, TimeStatistic timeStatistic) throws Throwable {
         long startTime = System.nanoTime();
-        pjp.proceed();
+        Object res = pjp.proceed();
         long endTime = System.nanoTime();
-        MethodSignature signature = (MethodSignature) pjp.getSignature();
-        Method method = signature.getMethod();
-        String message = method.getAnnotation(TimeStatistic.class).value();
-        log.info(message, (endTime - startTime) / 1000000);
+        log.info(timeStatistic.value(), (endTime - startTime) / 1000000);
+        return res;
     }
 }
